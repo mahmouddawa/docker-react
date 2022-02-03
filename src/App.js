56a7951091b142ccc3d1e889a2648a07fe9aa467
Mahmoud Dawa <mahmouddawa990@gmail.com>
2022-02-03 14:19:01 +0100

    testing the branch

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         Hiii!
+         Hiii from the branch!
         </p>
         <a
           className="App-link"
